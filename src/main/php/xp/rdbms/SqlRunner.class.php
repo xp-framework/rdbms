29920853da0396ee2b6610c6c2f5c34e53481020
U-UNITED\friebe <thekid@thekid.de>
2017-05-30 00:04:28 +0200

    Initial subcommand boilerplate

---
+++ src/main/php/xp/rdbms/SqlRunner.class.php
@@ -0,0 +1,14 @@
+<?php namespace xp\rdbms;
+
+class SqlRunner {
+
+  /**
+   * Runs SQL
+   *
+   * @param  string[] $args
+   * @return int exitcode
+   */
+  public static function main(array $args) {
+    return 0;
+  }
+}
