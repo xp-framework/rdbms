052d13319d1b0c6153e496c80f491a7c9e296ce8
U-UNITED\friebe <thekid@thekid.de>
2017-05-30 00:10:45 +0200

    First implementation w/ `xp sql <dsn> <statement>`

--- src/main/php/xp/rdbms/SqlRunner.class.php
+++ src/main/php/xp/rdbms/SqlRunner.class.php
@@ -1,5 +1,9 @@
 <?php namespace xp\rdbms;
 
+use rdbms\DriverManager;
+use util\profiling\Timer;
+use util\cmd\Console;
+
 class SqlRunner {
 
   /**
@@ -9,6 +13,28 @@ class SqlRunner {
    * @return int exitcode
    */
   public static function main(array $args) {
+    $dsn= $args[0];
+    $sql= $args[1];
+
+    $conn= DriverManager::getConnection($dsn);
+    $timer= (new Timer())->start();
+    try {
+      $q= $conn->query($sql);
+      if ($q->isSuccess()) {
+        Console::writeLinef('Query OK, %d rows affected (%.2f sec)', $q->affected(), $timer->elapsedTime());
+      } else {
+        $rows= 0;
+        while ($record= $q->next()) {
+          Console::writeLine($record);
+          $rows++;
+        }
+        Console::writeLinef('%d rows in set (%.2f sec)', $rows, $timer->elapsedTime());
+      }
+      $q->close();
+      Console::writeLine();
+    } catch (SQLException $e) {
+      Console::writeLine("\e[31m*** ", $e->compoundMessage(), "\e[0m");
+    }
     return 0;
   }
 }
