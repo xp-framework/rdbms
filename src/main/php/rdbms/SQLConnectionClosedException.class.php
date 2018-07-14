0e035a09b7d4ddca5ce6f378d2de054aef87efd5
U-UNITED\friebe <thekid@thekid.de>
2018-07-14 17:48:52 +0200

    Add number of tries to exception message

--- src/main/php/rdbms/SQLConnectionClosedException.class.php
+++ src/main/php/rdbms/SQLConnectionClosedException.class.php
@@ -1,13 +1,24 @@
 <?php namespace rdbms;
 
-
-
 /**
  * Indicates the connection was lost during an SQL query
  *
- * @see      rfc://0058
- * @purpose  Exception
+ * @see   rfc://0058
  */
 class SQLConnectionClosedException extends SQLStatementFailedException {
 
+  /**
+   * Constructor
+   *
+   * @param   string $message
+   * @param   int $tries
+   * @param   string $sql default NULL the SQL query string sent
+   * @param   int $errorcode default -1
+   */
+  public function __construct($message, $tries, $sql= null, $errorcode= -1) {
+    if ($tries > 1) {
+      $message.= ', retried '.($tries - 1).' times';
+    }
+    parent::__construct($message, $sql, $errorcode);
+  }
 }
