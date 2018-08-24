9430568c927a59e071ac0d0a8a16915c73bf6ba5
U-UNITED\friebe <thekid@thekid.de>
2018-08-24 18:24:08 +0200

    Rewrite documentation

--- src/main/php/rdbms/DriverManager.class.php
+++ src/main/php/rdbms/DriverManager.class.php
@@ -7,61 +7,44 @@
  * ====
  * The DriverManager class expects a unified connection string (we call 
  * it DSN) specifying the following: 
- * <ul>
- *   <li>The driver (here: <tt>sybase</tt>). This corresponds either to a 
- *       built-in driver class or one you have previously registered to it.
- *   </li>
- *   <li>An optional username and password (here: <tt>user</tt> and 
- *       <tt>pass</tt>).
- *   </li>
- *   <li>The hostname of the rdbms (here: <tt>server</tt>).
- *       Hostname is not completely correct: in SQLite, for example, this 
- *       specifies the name of the data file; in Sybase, it corresponds to 
- *       an entry in the interfaces file.
- *   </li>
- *   <li>The database name (here: <tt>NICOTINE</tt>).
- *       May be ommitted - for instance, Sybase offers a per-user default 
- *       database setting which automatically selects the specified database 
- *       after log in.
- *   </li>
- *   <li>Optional parameters (here: none).</li>
- * </ul>
+ *
+ * - The driver (here: `sybase`). This corresponds either to a built-in
+ *   driver class or one you have previously registered to it.
+ * - An optional username and password (here: `user` and `pass`).
+ * - The hostname of the rdbms (here: `server`). Hostname is not completely
+ *   correct: in SQLite, for example, this specifies the name of the data
+ *   file; in Sybase, it corresponds to an entry in the interfaces file.
+ * - The database name (here: `NICOTINE`).
+ *   May be ommitted - for instance, Sybase offers a per-user default 
+ *   database setting which automatically selects the specified database 
+ *   after log in.
+ * - Optional parameters (here: none).
+
  * Parameters in DSN are used in a key-value syntax as known from HTTP 
- * urls, e.g. <tt>mysql://user:pass@server?autoconnect=1</tt>.
+ * urls, e.g. `mysql://user:pass@server?autoconnect=1`.
  *
  * These parameters are recognized: 
- * <ul>
- *   <li>*autoconnect=value* - A call to rdbms.DBConnection#connect may be 
- *       ommitted. Just go ahead and when calling the first method which 
- *       needs to connect (and log in), a connection will be established.
- *       Value is an integer of either 1 (on) or 0 (off). Default is 0 (off). 
- *   </li>
- *   <li>*persistent=value* - Uses persistent database connections. These 
- *       are explained in the PHP manual. Value is an integer of either 1 
- *       (on) or 0 (off). Default is 0 (off). 
- *   </li>
- *   <li>*timeout=value* - Sets a connection timeout. Value is an integer 
- *       specifying the number of seconds to wait before cancelling a connect/ 
- *       log on procedure. Default may vary between different RDBMS. 
- *   </li>
- *   <li>*observer[key]=value* - Adds observers to the connection. The key 
- *       corresponds to an observer class, the value to a string passed to 
- *       its constructor. 
- *   </li>
- * </ul>
- * Note: For convenience, <tt>log=category</tt> is supported as an alias 
- * for <tt>observer[util.log.LogObserver]=category</tt>.
+ *
+ * - *autoconnect=value* - A call to rdbms.DBConnection#connect may be 
+ *   ommitted. Just go ahead and when calling the first method which 
+ *   needs to connect (and log in), a connection will be established.
+ *   Value is an integer of either 1 (on) or 0 (off). Default is 1 (on). 
+ * - *tz=Europe/Berlin* - Database server timezones, using Olson notation.
+ *   Default is to use current timezone, and not convert dates.
+ * - *timeout=value* - Sets a connection timeout. Value is an integer 
+ *   specifying the number of seconds to wait before cancelling a connect/ 
+ *   log on procedure. Default may vary between different RDBMS. 
  *
  * Usage
  * =====
- * <code>
- *   uses('rdbms.DriverManager');
+ * ```php
+ * use rdbms\DriverManager;
  *   
- *   $conn= DriverManager::getConnection('sybase://user:pass@server');
- *   $conn->connect();
+ * $conn= DriverManager::getConnection('sybase://user:pass@server');
+ * $conn->connect();
  *   
- *   Console::writeLine($conn->query('select @@version as version')->next('version'));
- * </code>
+ * Console::writeLine($conn->query('select @@version as version')->next('version'));
+ * ```
  *
  * @test     xp://net.xp_framework.unittest.rdbms.DriverManagerTest
  */
