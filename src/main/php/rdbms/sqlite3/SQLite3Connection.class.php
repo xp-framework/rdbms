fa2bbef0553a839402d57228b4ba5a86663581f8
Timm Friebe <thekid@thekid.de>
2023-11-19 14:16:55 +0100

    QA: Add all used classes to imports

--- src/main/php/rdbms/sqlite3/SQLite3Connection.class.php
+++ src/main/php/rdbms/sqlite3/SQLite3Connection.class.php
@@ -1,6 +1,18 @@
 <?php namespace rdbms\sqlite3;
 
-use rdbms\{DBConnection, QuerySucceeded, StatementFormatter, Transaction};
+use SQLite3, Exception;
+use lang\XPClass;
+use rdbms\{
+  DBConnection,
+  DBEvent,
+  DriverManager,
+  QuerySucceeded,
+  SQLConnectException,
+  StatementFormatter,
+  SQLStateException,
+  SQLStatementFailedException,
+  Transaction
+};
 
 /**
  * Connection to SQLite 3.x Databases via ext/sqlite3
@@ -18,27 +30,28 @@ use rdbms\{DBConnection, QuerySucceeded, StatementFormatter, Transaction};
  *
  * Therefore, this class offers a cast function which may be used
  * whithin the SQL as following:
- * <pre>
- *   select 
- *     cast(id, "int") id, 
- *     name, 
- *     cast(percentage, "float") percentage,
- *     cast(lastchange, "date") lastchange, 
- *     changedby
- *   from 
- *     test
- * </pre>
+ *
+ * ```sql
+ * select 
+ *   cast(id, "int") id, 
+ *   name, 
+ *   cast(percentage, "float") percentage,
+ *   cast(lastchange, "date") lastchange, 
+ *   changedby
+ * from 
+ *   test
+ * ```
  *
  * The resultset array will contain the following:
- * <pre>
- *   key          type
- *   ------------ -------------
- *   id           int
- *   name         string
- *   percentage   float
- *   lastchange   util.Date
- *   changedby    string
- * </pre>
+ * ```
+ * key          type
+ * ------------ -------------
+ * id           int
+ * name         string
+ * percentage   float
+ * lastchange   util.Date
+ * changedby    string
+ * ```
  *
  * @ext      sqlite3
  * @see      http://sqlite.org/
@@ -50,7 +63,7 @@ class SQLite3Connection extends DBConnection {
 
   static function __static() {
     if (extension_loaded('sqlite3')) {
-      \rdbms\DriverManager::register('sqlite+3', new \lang\XPClass(__CLASS__));
+      DriverManager::register('sqlite+3', new XPClass(__CLASS__));
     }
   }
 
@@ -72,30 +85,30 @@ class SQLite3Connection extends DBConnection {
    * @throws  rdbms.SQLConnectException
    */
   public function connect($reconnect= false) {
-    if ($this->handle instanceof \SQLite3) return true;  // Already connected
+    if ($this->handle instanceof SQLite3) return true;  // Already connected
     if (!$reconnect && (false === $this->handle)) return false;    // Previously failed connecting
 
     // Sanity check: SQLite(3) works local: either loads a database from a file
     // or from memory, so connecting to remote hosts is not supported, thus
     // checked here. You may pass "localhost", though
     if ('' != $this->dsn->getHost() && '.' != $this->dsn->getHost()) {
-      throw new \rdbms\SQLConnectException('sqlite+3:// connecting to remote database not supported', $this->dsn);
+      throw new SQLConnectException('sqlite+3:// connecting to remote database not supported', $this->dsn);
     }
 
-    $this->_obs && $this->notifyObservers(new \rdbms\DBEvent(\rdbms\DBEvent::CONNECT, $reconnect));
+    $this->_obs && $this->notifyObservers(new DBEvent(DBEvent::CONNECT, $reconnect));
     $database= (string)urldecode((string)$this->dsn->getDatabase());
     if (0 === strlen($database) || false !== strpos($database, "\0")) {
-      throw new \rdbms\SQLConnectException('Illegal filename', $this->dsn);
+      throw new SQLConnectException('Illegal filename', $this->dsn);
     }
 
     try {
-      $this->handle= new \SQLite3($database, SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
-    } catch (\Exception $e) {
-      throw new \rdbms\SQLConnectException($e->getMessage().': '.$database, $this->dsn);
+      $this->handle= new SQLite3($database, SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
+    } catch (Exception $e) {
+      throw new SQLConnectException($e->getMessage().': '.$database, $this->dsn);
     }
     
     $this->getFormatter()->dialect->registerCallbackFunctions($this->handle);
-    $this->_obs && $this->notifyObservers(new \rdbms\DBEvent(\rdbms\DBEvent::CONNECTED, $reconnect));
+    $this->_obs && $this->notifyObservers(new DBEvent(DBEvent::CONNECTED, $reconnect));
 
     return true;
   }
@@ -119,7 +132,7 @@ class SQLite3Connection extends DBConnection {
    * @throws  rdbms.SQLStatementFailedException
    */
   public function selectdb($db) {
-    throw new \rdbms\SQLStatementFailedException(
+    throw new SQLStatementFailedException(
       'Cannot select database, not implemented in SQLite'
     );
   }
@@ -130,9 +143,9 @@ class SQLite3Connection extends DBConnection {
    * @return  var identity value
    */
   public function identity($field= null) {
-    if (!$this->handle instanceof \SQLite3) throw new \rdbms\SQLStateException('Not connected');
+    if (!$this->handle instanceof SQLite3) throw new SQLStateException('Not connected');
     $i= $this->handle->lastInsertRowID();
-    $this->_obs && $this->notifyObservers(new \rdbms\DBEvent(\rdbms\DBEvent::IDENTITY, $i));
+    $this->_obs && $this->notifyObservers(new DBEvent(DBEvent::IDENTITY, $i));
     return $i;
   }
 
@@ -145,11 +158,11 @@ class SQLite3Connection extends DBConnection {
    * @throws  rdbms.SQLException
    */
   protected function query0($sql, $buffered= true) {
-    $this->handle instanceof \SQLite3 || $this->connections->establish($this);
-    
+    $this->handle instanceof SQLite3 || $this->connections->establish($this);
+
     $result= $this->handle->query($sql);
     if (false === $result) {
-      $e= new \rdbms\SQLStatementFailedException(
+      $e= new SQLStatementFailedException(
         'Statement failed: '.$this->handle->lastErrorMsg().' @ '.$this->dsn->getDatabase(),
         $sql, 
         $this->handle->lastErrorCode()
